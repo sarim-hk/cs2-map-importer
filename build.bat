3d70277a7e4c96db9e50ae6741c3d6675283d75b
sarim-hk <shktv03@gmail.com>
2023-07-02 15:56:55 +0100

    build tools

---
+++ build.bat
@@ -0,0 +1,16 @@
+:: PRE CLEANUP
+del /f /s /q release
+rmdir release
+
+:: BUILD RELEASE FILES
+pyinstaller --onefile src/cs2importer.py
+type nul >dist/cs2importer.cfg
+
+::: ZIP RELEASE FILES
+ren dist cs2importer
+7z a cs2importer/cs2importer.zip cs2importer
+ren cs2importer release
+
+:: POST CLEANUP
+rmdir build /S /Q
+del cs2importer.spec
